33dd5d6d4cd0e90b44ea459aec7d843634e92b07
pjkee <pjkee99@hotmail.com>
2024-09-07 17:43:59 +0800

    using bloc to fetch data and display

---
+++ lib/features/daily_news/presentation/pages/home/daily_news.dart
@@ -0,0 +1,54 @@
+import 'package:clean_architecture/features/daily_news/presentation/bloc/article/remote/remote_article_bloc.dart';
+import 'package:clean_architecture/features/daily_news/presentation/bloc/article/remote/remote_article_state.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class DailyNews extends StatelessWidget {
+  const DailyNews({super.key});
+
+  _buildAppBar() {
+    return AppBar(
+      title: const Text(
+        'Daily News',
+        style: TextStyle(color: Colors.black),
+      ),
+    );
+  }
+
+  _buildBody() {
+    return BlocBuilder<RemoteArticleBloc, RemoteArticleState>(
+        builder: (_, state) {
+      if (state is RemoteArticlesLoading) {
+        return const Center(
+          child: CupertinoActivityIndicator(),
+        );
+      }
+      if (state is RemoteArticlesError) {
+        return const Center(
+          child: Icon(Icons.refresh),
+        );
+      }
+      if (state is RemoteArticlesDone) {
+        return ListView.builder(
+          itemBuilder: (ctx, index) {
+            return ListTile(
+              title: Text('$index'),
+            );
+          },
+          itemCount: state.articles?.length ?? 0,
+        );
+      }
+      return const SizedBox();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _buildAppBar(),
+      body: _buildBody(),
+    );
+  }
+}
